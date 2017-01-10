7a5e1ae1e9627db157577ca4c37e7887f54dc1c3
medakk <karanth.karthik@gmail.com>
2017-01-10 21:45:38 +0530

    Used threading module to run network operations in parallel

--- dark-scrape.py
+++ dark-scrape.py
@@ -6,16 +6,65 @@
 # Usage: dark-scraper.py <url to artist page> [<output file>]
 # or:    dark-scraper.py <artist name>        [<output file>]
 
-# TODO: Handle exceptions more gracefully
-
 import sys
 import re
+import threading
+from queue import PriorityQueue
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 if len(sys.argv)!=3 and len(sys.argv)!=2:
     print("Usage: {} <URL to artist page> [<output file>]".format(sys.argv[0]), file=sys.stderr)
     sys.exit(1)
+
+# Thread-safe print function
+__print_lock = threading.Lock()
+__p = print
+def print(*args, **kwargs):
+    with __print_lock:
+        __p(*args, **kwargs)
+
+# Given a string containing the html of an album, scrape
+# the lyrics from it
+def scrape_from_html(album_html):
+    text = "" # Store scraped text in this
+
+    soup = BeautifulSoup(album_html, "html.parser")
+    header = soup.find("h2").get_text()
+    content = soup.find("div", class_="lyrics")
+    try:
+        content.find("div", class_="thanks").decompose()
+    except:
+        pass # no thanks found. No problem, we were trying
+             # to get rid of it anyway
+    try:
+        content.find("div", class_="note").decompose()
+    except:
+        pass # no "note" found. No problem, we were trying
+             # to get rid of it anyway
+
+    #get rid of the "ARTIST LYRICS" thing
+    regex = re.compile(r'[A-Z ]*LYRICS')
+    blocks = regex.split(content.get_text())
+
+    text = text + '\n' + "*"*(len(header)+4)
+    text = text + '\n' + "* "+header+" *"
+    text = text + '\n' + "*"*(len(header)+4)
+
+    for block in blocks:
+        text = text + '\n' + block
+
+    return text
+
+# called by each thread
+def get_url(q, url, index):
+    print("Scraping: {}".format(url), file=sys.stderr)
+    try:
+        album_html = urlopen(url).read().decode("utf-8")
+        q.put((index, album_html))
+    except Exception as e:
+        print("get_url: Error in page: {}".format(url), file=sys.stderr)
+        print(e, file=sys.stderr)
 
 # Figure out the URL to use
 source_arg = sys.argv[1]
@@ -28,6 +77,8 @@ elif source_arg.startswith("darklyrics.com"):
 else:
     source_arg = source_arg.lower().replace(' ', '')
     url = "http://www.darklyrics.com/{}/{}.html".format(source_arg[0], source_arg)
+
+# Read artist page
 print("Accessing {}".format(url), file=sys.stderr)
 
 with urlopen(url) as ufd:
@@ -38,55 +89,37 @@ artist_mo = artist_re.findall(artist_html)
 
 album_urls = [s.replace("..", "http://www.darklyrics.com")[1:-3] for s in artist_mo]
 
+# Create threads to download and scrape each page
+q = PriorityQueue()
+threads = []
+for i,url in enumerate(album_urls):
+    t = threading.Thread(target=get_url, args=(q, url, i))
+    t.daemon = True
+    t.start()
+    threads.append(t)
+
+# Wait for all urls to download
+for t in threads:
+    t.join()
+
+# File to store output in
 file_name=None
 if len(sys.argv)==2:
     artist_soup = BeautifulSoup(artist_html, "html.parser")
     file_name = artist_soup.find("title").get_text() + ".txt"
 else:
     file_name = sys.argv[2]
-
 fd = open(file_name, "w")
-error_urls = []
 
-for url in album_urls:
+# Go through the queue
+while not q.empty():
     try:
-        print("Scraping: " + url, file=sys.stderr)
-        with urlopen(url) as ufd:
-            album_html = ufd.read()
-
-        soup = BeautifulSoup(album_html, "html.parser")
-
-        header = soup.find("h2").get_text()
-
-        content = soup.find("div", class_="lyrics")
-        try:
-            content.find("div", class_="thanks").decompose()
-        except:
-            pass # no thanks found. No problem, we were trying
-                 # to get rid of it anyway
-        try:
-            content.find("div", class_="note").decompose()
-        except:
-            pass # no "note" found. No problem, we were trying
-                 # to get rid of it anyway
-
-        #get rid of the "ARTIST LYRICS" thing
-        regex = re.compile(r'[A-Z ]*LYRICS')
-        text = regex.split(content.get_text())
-
-        print("*"*(len(header)+4), file=fd)
-        print("* "+header+" *", file=fd)
-        print("*"*(len(header)+4), file=fd)
-
-        for block in text:
-            fd.write(block)
+        index,album_html = q.get()
+        text = scrape_from_html(album_html)
+        print(text, file=fd)
     except Exception as e:
-        print("Error in page: {}".format(url), file=sys.stderr)
+        print("Error scraping from html: {}".format(album_html), file=sys.stderr)
         print(e, file=sys.stderr)
-        error_urls.append(url)
 
-if error_urls:
-    print("\nError parsing the following pages:",file=sys.stderr)
-    print(*error_urls, sep='\n',file=sys.stderr)
 
 fd.close()
